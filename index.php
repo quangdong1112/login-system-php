e7ab287b43ea9f3ce43493972b4fd7bc2f2331c1
quangdong1112 <quangdong102@gmail.com>
2022-02-27 16:12:38 +0700

    Update file font-end

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home Page Login System</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+    <div class="wrap">
+        <nav class="nav nav-custom">
+            <h1>Login</h1>
+            <form action="" class="form-signin">
+                <input type="text" name="user_name" placeholder="Username/Email">
+                <input type="password" name="user_pswd" placeholder="Password">
+                <button type="submit" name="login" class="login">Login</button>
+            </form>
+            <a href="signup.php" class="btn btn-red signup">Register</a>
+        </nav>
+        <section class="">
+            <h3>Home</h3>
+        </section>
+        <footer>
+            <p>Designed by <a href="#">Quang Đồng</a</p>
+        </footer>
+    </div>
+</body>
+</html>
---
+++ signup.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home Page Login System</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+    <div class="wrap">
+        <nav class="nav nav-custom">
+            <h1>Login</h1>
+            <form action="" class="form-signin">
+                <input type="text" name="user_name" placeholder="Username/Email">
+                <input type="password" name="user_pswd" placeholder="Password">
+                <button type="submit" name="login" class="login">Login</button>
+            </form>
+        </nav>
+        <section class="">
+            <h3>Sigup</h3>
+            <div class="form-signup">
+                <form action="">
+                    <input type="text" name="user_first" placeholder="First Name">
+                    <input type="text" name="user_last" placeholder="Last Name">
+                    <input type="email" name="user_email" placeholder="Email">
+                    <input type="text" name="user_name" placeholder="Username">
+                    <input type="password" name="user_pswd" placeholder="Password">
+                    <input type="password" name="user_pswd" placeholder="Password replace">
+                    <button class="btn btn-signup" name="btn-signup" type="submit">Register</button>
+                </form>
+            </div>
+        </section>
+        <footer>
+            <p>Designed by <a href="#">Quang Đồng</a</p>
+        </footer>
+    </div>
+</body>
+</html>
